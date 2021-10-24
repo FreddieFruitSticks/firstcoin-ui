80bf071ad7c2c4bb05bddde6d0d71a4923c8306e
freddie <freddieodonnell@gmail.com>
2021-10-24 11:18:06 +0200

    wallet stylingf

--- src/components/wallet.tsx
+++ src/components/wallet.tsx
@@ -6,16 +6,36 @@ interface IWallet{
 }
 
 const Wallet = ({colour}: IWallet) => {
+    const copy = () => {}
     return (
         <div className="rounded-md text-white p-4 min-h-100 w-4/5 bg-trendyBlue mb-10">
-            <div>
-                user abcxyz
+            <div className="justify-content flex space-x-4 pb-4">
+                <div className="flex items-center">
+                    <div>
+                        user abcxyz
+                    </div>
+                </div>
+                <div onClick={copy} className={`h-10 w-16 bg-trendyYellow text-white flex justify-center items-center transform transition duration-500 hover:scale-105 cursor-pointer font-semibold py-2 px-4 rounded`}>
+                    Copy
+                </div> 
+                
             </div>
-            <div className="">
-            <div className="flex align-center space-between">
-                <div className="text-white mr-4 flex flex-col justify-center"><div className="">Pay to:</div></div>
-                <textarea className="form-textarea mt-1 block w-9/12 border-white overflow-hidden" rows={1} placeholder="Public key..."></textarea>
+            <div className="mb-4">
+                <div className="flex align-center space-between">
+                    <div className="text-white mr-4 flex flex-col justify-center"><div className="">Pay to:</div></div>
+                    <textarea className="form-textarea mt-1 block w-9/12 border-white overflow-hidden" rows={1} placeholder="Public key..."></textarea>
+                </div>
             </div>
+            <div className="justify-content flex space-x-4 pb-4">
+                <div className="flex items-center">
+                    <div>
+                        Balance: 
+                    </div>
+                </div>
+                <div>
+                    22
+                </div> 
+                
             </div>
         </div>
     )
