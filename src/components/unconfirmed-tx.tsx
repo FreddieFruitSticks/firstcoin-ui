05a8e34d37af7cb84ea15600e3c8116fed9877aa
Freddie <freddieodonnell@gmail.com>
2022-10-13 12:02:10 +0200

    fix ui

--- src/components/unconfirmed-tx.tsx
+++ src/components/unconfirmed-tx.tsx
@@ -5,15 +5,15 @@ const UnconfirmedTx = ({unconfirmedTxData}: {unconfirmedTxData: ITransaction}) =
     return(
         <div className="border border-black w-11/12 mb-10 bg-white">
             <div className="p-2">
-                <span className="font-bold">In tx:</span>: {formatKey(unconfirmedTxData.vin[0].txid)}
+                <span className="font-bold">In tx:</span>{formatKey(unconfirmedTxData.vin[0].txid)}
             </div>
             <div className="p-2">
-                <span className="font-bold">pay to:</span> {formatKey(Buffer.from(unconfirmedTxData.vout[0].scriptPubKey, 'base64').toString('ascii'))}  <span className="font-bold">amount: </span>: {unconfirmedTxData.vout[0].value}
+                <span className="font-bold">pay to:</span> {formatKey(Buffer.from(unconfirmedTxData.vout[0].scriptPubKey, 'base64').toString('ascii'))}  <span className="font-bold">amount: </span>{unconfirmedTxData.vout[0].value}
             </div>
             <div className="p-2">
                  {unconfirmedTxData?.vout[1]&&
                      <>
-                        <span className="font-bold">pay from:</span> {formatKey(Buffer.from(unconfirmedTxData?.vout[1]?.scriptPubKey, 'base64').toString('ascii'))}  <span className="font-bold">vout:</span>: {unconfirmedTxData?.vout[1]?.value}
+                        <span className="font-bold">pay from:</span> {formatKey(Buffer.from(unconfirmedTxData?.vout[1]?.scriptPubKey, 'base64').toString('ascii'))}  <span className="font-bold">tx out: </span>{unconfirmedTxData?.vout[1]?.value}
                      </>
                 }
             </div>
