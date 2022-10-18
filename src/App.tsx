530e9dbd758d8690dce27082821fc0c5aed86d22
Freddie <freddieodonnell@gmail.com>
2022-10-18 23:53:25 +0200

    using mobile phone

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,16 @@ import { Context } from './context/context-provider';
 import Blockchain from './components/blockchain';
 import ControlPanel from './components/control-panel';
 import StatusMessage from './components/status-message';
+import React, {useEffect, useState} from "react";
+import MyModal from './components/modal';
 
 function App({state, dispatch}: Context) {
   return (
     <div className="bg-background">
       <div className="w-8/12">
+        {useCheckMobileScreen() &&
+          <MyModal/>
+        }
         <StatusMessage {...{state, dispatch}}/>
         <Header {...{state, dispatch}}/>
         <div className="pt-28">
@@ -21,5 +26,24 @@ function App({state, dispatch}: Context) {
     </div>
   );
 }
+
+
+
+const useCheckMobileScreen = () => {
+    const [width, setWidth] = useState(window.innerWidth);
+    const handleWindowSizeChange = () => {
+            setWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowSizeChange);
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        }
+    }, []);
+
+    return (width <= 768);
+}
+
 
 export default connect(App);
---
+++ src/components/modal.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+const MyModal = ({}) => {
+    const customStyles = {
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+        },
+      };
+    let subtitle: HTMLHeadingElement | null;
+    const [modalIsOpen, setIsOpen] = useState(true);
+    const [modalHasOpened, setModalHasOpened] = useState(false);
+  
+    function openModal() {
+        if (!modalHasOpened){
+            setIsOpen(true);
+            setModalHasOpened(true)
+        }
+    }
+
+  
+    function closeModal() {
+      setIsOpen(false);
+    }
+    return (
+        <div>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <h2 className="text-trendyRed font-bold mb-4">Welcome to Firstcoin!</h2>
+                <div>I see you are on a mobile phone. Please use landscape mode to use this app. Happy mining!</div>
+                {/* <button onClick={closeModal}>close</button> */}
+                <button onClick={closeModal} className={`h-10 w-16 mt-4 focus:ring-4 focus:ring-white bg-trendyRed text-white flex justify-center items-center transform transition duration-500 hover:scale-105 cursor-pointer font-semibold py-2 px-4 rounded`}>
+                    Close
+                </button> 
+            </Modal>
+        </div>
+
+    )
+}
+
+export default MyModal
